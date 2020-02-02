b747003e8908752ebc45f24d91ffaa18bac11f92
devleni <50588358+devleni@users.noreply.github.com>
2020-02-02 19:05:08 +0100

    add ui and hard-coded data

---
+++ app.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    name: "John Doe",
+    age: 32,
+    gender: "male",
+    lookingfor: "female",
+    location: "Boston MA",
+    image: "https://randomuser.me/api/portraits/men/82.jpg"
+  },
+  {
+    name: "Jen Smith",
+    age: 26,
+    gender: "female",
+    lookingfor: "female",
+    location: "Maimi FL",
+    image: "https://randomuser.me/api/portraits/women/82.jpg"
+  },
+  {
+    name: "William Johnson",
+    age: 38,
+    gender: "male",
+    lookingfor: "male",
+    location: "Lynn Ma",
+    image: "https://randomuser.me/api/portraits/men/83.jpg"
+  },
+  {
+    name: "Sarah Miller",
+    age: 34,
+    gender: "female",
+    lookingfor: "male",
+    location: "NYC",
+    image: "https://randomuser.me/api/portraits/women/4.jpg"
+  },
+];
